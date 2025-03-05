0d95e35c1004a3b4f4cf63dfc6fd888f35120863
varun <varunsahil10@gmail.com>
2025-03-06 02:02:51 +0530

    SearchVector full text search

--- fulltextsearch/views.py
+++ fulltextsearch/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
 from .models import *
+
 # Create your views here.
 
 def home(request):
     products = Product.objects.all()
     # print(request)
-    query = ''
-    if request.method == 'GET':
-        print(request.GET)
-        query = request.GET.get('q')
-        if query:
-            products = products.filter(description__search=query)
+
+    q = request.GET.get('q','')
+    if q:
+        
+        '''
+        simple full text search on a single field
+
+        1. for searching on only and only 1 field
+        2. blues, blueing ..
+        '''
+        # products = products.filter(description__search=q)
+    
+
+        '''
+        full text search using postgres module (Using SearchVector only)
+
+        1. for searching on more than 1 field
+        2. blues, blueing ..
+        '''
+        vector = SearchVector('name','description')
+        products = products.annotate(
+            search = vector
+        ).filter(search=q)
+    
     context = {
         'products': products,
-        'q':query,
+        'q':q,
     }
     return render(request,'index.html',context)
