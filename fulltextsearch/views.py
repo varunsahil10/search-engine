404151864188a4754e413a81963d84e0b777e053
varun <varunsahil10@gmail.com>
2025-03-06 14:50:33 +0530

    Search using SearchRank

--- fulltextsearch/views.py
+++ fulltextsearch/views.py
@@ -11,6 +11,8 @@ def home(request):
     q = request.GET.get('q','')
     if q:
         
+        #FTS ignores articles and single words
+
         '''
         simple full text search on a single field
 
@@ -26,10 +28,37 @@ def home(request):
         1. for searching on more than 1 field
         2. blues, blueing ..
         '''
+        # vector = SearchVector('name','description')
+        # query = q
+    
+        # products = products.annotate(search = vector).filter(search=query)
+
+        '''
+        full text search using postgres module (Using SearchVector and SearchQuery only)
+
+        1. for searching on more than 1 field
+        2. blues, blueing ..
+        '''
+        # vector = SearchVector('name','description')
+        # query = SearchQuery(q)
+
+        # products = products.annotate(search = vector).filter(search=query)
+
+        '''
+        full text search using postgres module (Using SearchVector and SearchQuery and SearchRank)
+
+        1. for searching on more than 1 field
+        2. blues, blueing ..
+        '''
         vector = SearchVector('name','description')
-        products = products.annotate(
-            search = vector
-        ).filter(search=q)
+        query = SearchQuery(q)
+        rank = SearchRank(vector,query)
+
+        products = products.annotate(rank = rank).filter(rank__gte=0.001).order_by('-rank')
+
+        print(vector)
+        print(query)
+        print(rank)
     
     context = {
         'products': products,
