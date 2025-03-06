a77c8a190f4b6b32eca09c4b6e15ab781d309c9a
varun <varunsahil10@gmail.com>
2025-03-07 02:08:27 +0530

    Django admin

--- fulltextsearch/admin.py
+++ fulltextsearch/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pid','name','brand','gender','price','description','color',)
+
+    search_fields = ('pid','name','brand','gender','price','description','color',)
+
+    list_filter = ('brand','gender','color',)
+
+    readonly_fields = ('name','description')
+    exclude = ('pid',)
+
+    actions = ['mark_color_peach']
+
+    def mark_color_peach(self,request,queryset):
+        queryset.update(
+            color = 'Peach'
+        )
